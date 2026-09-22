5319f0d43285682252e79f1d84445be59cbdb4d4
gopinath-33 <gopinath04160@gmail.com>
2026-09-22 13:37:15 +0530

    Fix project and certificate routes and layout structure

--- blog-project-starter-frontend/src/App.js
+++ blog-project-starter-frontend/src/App.js
@@ -1,34 +1,32 @@
-import Home from "./components/Home";
-
-import About from "./components/About";
-import Project from './components/Project'
-import Certificate from './components/Certificate'
-import Contact from "./components/Contact";
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Project from './components/Project';
+import Certificate from './components/Certificate';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="w-full min-h-screen bg-slate-950 text-slate-100 overflow-x-hidden m-0 p-0">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
         
-          <Route path="/about" element={<About />} />
+        {/* Project & Projects rendu path-ukum work aagum */}
+        <Route path="/projects" element={<Project />} />
+        <Route path="/project" element={<Project />} />
 
-          <Route path="/project" element={<Project />} />
-          <Route path="/certificate" element={<Certificate />} />
-          <Route path="/contact" element={<Contact />} />
-         
-        </Routes>
-      </BrowserRouter>
-    </div>
+        {/* Certificate & Certificates rendu path-ukum work aagum */}
+        <Route path="/certificates" element={<Certificate />} />
+        <Route path="/certificate" element={<Certificate />} />
+
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
-
-
-
-
 
 export default App;
